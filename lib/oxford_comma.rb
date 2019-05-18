a0fa578f27b09f033ef728361c686d5798def579
Jack Atherton <jackrrye@gmail.com>
2019-05-18 17:01:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
   if array.size == 1 
     array.join 
   elsif array.size ==2 
-    
+    array = array.insert(1, " and ")
+    array.join
   else
     array.each do |x|
       y = x << ", "
